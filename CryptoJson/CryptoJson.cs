8eb0dcb362eef64204b96c7a056878f8bf0a952b
Andreas Jakof <andreas.jakof@basf.com>
2019-10-18 16:49:41 +0200

    Init

---
+++ CryptoJson/CryptoJson.cs
@@ -0,0 +1,193 @@
+﻿using BASF.ExtensionLibrary.Strings;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Bson;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+#pragma warning disable 618
+namespace BASF.ExtensionLibrary.Security
+{
+    /// <summary>
+    /// The CrytoCredentials are the basis for storing Credentials securely in Source Code/Web.config
+    /// </summary>
+    public class CryptoJson<T> where T : CryptoJson<T>
+    {
+        public static X509Certificate2 LoadCert(X509FindType x509FindType, object findValue)
+        {
+            using (var store = new X509Store(StoreName.My, StoreLocation.CurrentUser))
+            {
+                store.Open(OpenFlags.ReadOnly);
+                var cert = store.Certificates.Find(x509FindType, findValue, false).OfType<X509Certificate2>().FirstOrDefault();
+                if (cert != null)
+                {
+                    return cert;
+                }
+            }
+            using (var store = new X509Store(StoreName.My, StoreLocation.LocalMachine))
+            {
+                store.Open(OpenFlags.ReadOnly);
+                return store.Certificates.Find(x509FindType, findValue, false).OfType<X509Certificate2>().FirstOrDefault();
+            }
+        }
+
+        private static U Encrypt<U>(Func<RSA, int, U> encryptionFunc, X509Certificate2 certificate)
+        {
+            
+            var rsa = certificate.GetRSAPublicKey();
+            int chunksize = (rsa.KeySize / 8) - 66; //Asymmetric Encryption using OAEP256: maxLen = KeySize in Bytes - 2*HashSize in Bytes - 2 => KeySize - 2*32 - 2 = Keysize - 66
+
+            return encryptionFunc(rsa, chunksize);
+        }
+        public string ToBase64(X509FindType x509FindType, object findValue)
+        {
+            var cert = LoadCert(x509FindType, findValue);
+            return ToBase64(cert);
+        }
+        /// <summary>
+        /// Encrypts to current Object Asymmetrically (RSA OAEP256) into an BASE64 String.<br />
+        /// Use <see cref="Load{T}(string)"/> to load this again.
+        /// </summary>
+        /// <returns>BASE64 encoded RSA encrypted credentials</returns>
+        public string ToBase64(X509Certificate2 certificate)
+        {
+            return Encrypt((rsa, chunksize) =>
+            {
+                var json = JsonConvert.SerializeObject(this);
+                var bytes = Encoding.UTF8.GetBytes(json);
+                int count = bytes.Count();
+                List<String> data = new List<String>();
+                for (int i = 0; i < count; i = i + chunksize)
+                {
+                    var buffer = bytes.Skip(i).Take(chunksize).ToArray();
+                    data.Add(Convert.ToBase64String(rsa.Encrypt(buffer, RSAEncryptionPadding.OaepSHA256)));
+                }
+                return StringExtensions.ToStringList(data);
+            }, certificate);
+        }
+
+        public MemoryStream ToBson(X509FindType x509FindType, object findValue)
+        {
+            var cert = LoadCert(x509FindType, findValue);
+            return ToBson(cert);
+        }
+
+        /// <summary>
+        /// Encrypts to current Object Asymmetrically (RSA OAEP256) into an binary bson representation.<br />
+        /// Use <see cref="Load{T}(byte[])"/> to load this again.
+        /// </summary>
+        /// <param name="dic">Key Value Store of all necessary members to restore the credentials from.</param>
+        /// <returns>BASE64 encoded RSA encrypted credentials</returns>
+        public MemoryStream ToBson(X509Certificate2 certificate) 
+        {
+            return Encrypt((rsa, chunksize) =>
+            {
+                using (MemoryStream mem = new MemoryStream())
+                using (BsonWriter bsw = new BsonWriter(mem))
+                {
+                    JsonSerializer serializer = new JsonSerializer();
+                    serializer.Serialize(bsw, this);
+
+                    mem.Position = 0;
+
+                    MemoryStream enc = new MemoryStream((int)Math.Ceiling(mem.Length / (double)chunksize) * (chunksize + 66));
+                    using (BinaryReader br = new BinaryReader(mem))
+                    {
+                        byte[] buffer;
+                        while ((buffer = br.ReadBytes(chunksize)).Length > 0)
+                        {
+                            var encData = rsa.Encrypt(buffer, RSAEncryptionPadding.OaepSHA256);
+                            enc.Write(encData,0,encData.Length);
+                        }
+                        enc.Flush();
+                        enc.Position = 0;
+                        return enc;
+                    }
+                }
+                
+            }, certificate);
+        }
+
+        public static T Load(X509FindType x509FindType, object findValue, String encryptedData)
+        {
+            var cert = LoadCert(x509FindType, findValue);
+            return Load(cert, encryptedData);
+        }
+
+        /// <summary>
+        /// Loads RSA encrypted BASE64 encoded String of your Object <see cref="ToBase64()"/> 
+        /// </summary>
+        /// <typeparam name="T">Type of your Credentials. Make sure to set the Certificate</typeparam>
+        /// <param name="encryptedData">BASE64 encoded RSA encrypted json of object</param>
+        /// <returns>decrypted credentials</returns>
+        public static T Load(X509Certificate2 certificate, String encryptedData)
+        {
+            return Load((rsa, chunksize) =>
+            {
+                List<string> data = encryptedData.ToList();
+                using (MemoryStream mem = new MemoryStream(data.Count * chunksize))
+                {
+                    using (BinaryWriter bw = new BinaryWriter(mem))
+                    {
+                        foreach (byte[] buffer in data.Select(x => Convert.FromBase64String(x)))
+                        {
+                            bw.Write(rsa.Decrypt(buffer, RSAEncryptionPadding.OaepSHA256));
+                        }
+                        bw.Flush();
+                        mem.Position = 0;
+                        var json = Encoding.UTF8.GetString(mem.ToArray());
+
+                        return JsonConvert.DeserializeObject<T>(json);
+                    }
+                }
+            }, certificate);
+        }
+
+        public static T Load(X509FindType x509FindType, object findValue, Stream encryptedData)
+        {
+            var cert = LoadCert(x509FindType, findValue);
+            return Load(cert, encryptedData);
+        }
+
+        /// <summary>Loads RSA encrypted BASE64 encoded String of your Object <see cref="M:BASF.ExtensionLibrary.Security.CryptoJson`1.ToBase64(X509Certificate2)"/></summary>
+        /// <param name="certificate">the X509Certificate used to encrypt the data in the Stream </param>
+        /// <param name="encryptedData">BASE64 encoded RSA encrypted json (BSON) of object</param>
+        /// <returns>decrypted credentials</returns>
+        public static T Load(X509Certificate2 certificate, Stream encryptedData)
+        {
+            return Load((rsa, chunksize) =>
+            {
+                using (BinaryReader br = new BinaryReader(encryptedData))
+                using (MemoryStream mem = new MemoryStream((int)encryptedData.Length))
+                using (BinaryWriter bw = new BinaryWriter(mem))
+                {
+                    byte[] buffer;
+                    while ((buffer = br.ReadBytes(chunksize)).Length == chunksize)
+                    {
+                        bw.Write(rsa.Decrypt(buffer, RSAEncryptionPadding.OaepSHA256));
+                    }
+                    bw.Flush();
+                    mem.Position = 0;
+                    using (BsonReader bsonReader = new BsonReader(mem))
+                    {
+                        JsonSerializer serializer = new JsonSerializer();
+                        return serializer.Deserialize<T>(bsonReader);
+                    }
+                }
+            }, certificate);
+        }
+
+        private static T Load(Func<RSA, int, T> decryptFunc, X509Certificate2 certificate)
+        {
+          
+            var rsa = certificate.GetRSAPrivateKey();
+            int chunksize = (rsa.KeySize / 8);
+            return decryptFunc(rsa, chunksize);
+        }
+    }
+}
+#pragma warning restore 618
+
---
+++ CryptoJson/StringExtensions.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Text;
+
+namespace BASF.ExtensionLibrary.Strings
+{
+    /// <summary>
+    /// Class StringExtensions.
+    /// </summary>
+    public static class StringExtensions
+    {
+
+        public static StringBuilder AppendJoin<T>(this StringBuilder sb, char delimiter, IEnumerable<T> enumerable)
+        {
+            if (enumerable.Any())
+            {
+                sb.Append(enumerable.First());
+                foreach (var o in enumerable.Skip(1))
+                {
+                    sb.AppendFormat("{0}{1}", delimiter, o);
+                }
+            }
+            return sb;
+        }
+
+        /// <summary>
+        /// Converts this string to it's secure equivalent
+        /// </summary>
+        /// <param name="unsecure">The input string</param>
+        /// <returns>a secure string containing the characters from the unsecure string</returns>
+        public static SecureString ToSecureString(this string unsecure)
+        {
+            SecureString secure = new SecureString();
+            foreach (char c in unsecure) {
+                secure.AppendChar(c);
+            }
+
+            return secure;
+        }
+
+        /// <summary>
+        /// Converts this secure string to it's unsecure equivalent
+        /// </summary>
+        /// <param name="secure">The input secure string</param>
+        /// <returns>an unsecure string, containing the characters from the secure string</returns>
+        public static string ToUnSecureString(this SecureString secure) => 
+            Marshal.PtrToStringUni(Marshal.SecureStringToGlobalAllocUnicode(secure));
+
+
+        /// <summary>
+        /// Converts objects to a delimited stringlist using "ToString()".
+        /// </summary>
+        /// <typeparam name="T">type of the objects</typeparam>
+        /// <param name="objects">The objects</param>
+        /// <param name="delimiter">The delimiter.</param>
+        /// <returns>List of String</returns>
+        public static String ToStringList<T>(this IEnumerable<T> objects, Char delimiter = ',') => 
+            new StringBuilder().AppendJoin(delimiter, objects).ToString();
+
+        /// <summary>
+        /// Converts a delimited stringlist back to a List of string.
+        /// </summary>
+        /// <param name="stringList">The delimited string list.</param>
+        /// <param name="delimiter">The delimiter</param>
+        /// <returns>List&lt;String&gt;.</returns>
+        public static List<String> ToList(this string stringList, Char delimiter = ',') => 
+            stringList.Split(delimiter).ToList();
+
+        /// <summary>
+        /// Comverts a delimited stringlist back to its original form of a list
+        /// </summary>
+        /// <typeparam name="T">Type of the objects in the resulting list</typeparam>
+        /// <param name="stringList">delimited list of strings</param>
+        /// <param name="converterFunction">function to convert the resulting string into the object type</param>
+        /// <param name="delimiter">delimiter</param>
+        /// <returns>List of the objects</returns>
+        public static List<T> ToList<T>(this string stringList, Func<string, T> converterFunction, Char delimiter = ',') => 
+            stringList.Split(delimiter).Select(converterFunction).ToList();
+        
+    }
+}
